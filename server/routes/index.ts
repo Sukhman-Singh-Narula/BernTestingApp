dc7e82e47dc31245256c1da7fd72c40dc22428f2
edmaclean <38720084-edmaclean@users.noreply.replit.com>
2025-03-14 11:40:05 +0000

    Restored to 'db248da44b679da461a18ca133e95199d0e4e366'
    Replit-Restored-To: db248da44b679da461a18ca133e95199d0e4e366

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express';
 import messagesRoutes from './messagesRoutes';
 import conversationRoutes from './conversationRoutes';
-import sseRoutes from './sseRoutes';
 
 const router = Router();
 
 // Register routes
 router.use(conversationRoutes);
 router.use(messagesRoutes);
-router.use(sseRoutes);
 
 export { router };
